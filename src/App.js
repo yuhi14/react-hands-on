e26b8755ffda6a35968fcdafdf005fefc5a53823
永田悠飛 <yuhi14skywalker@gmail.com>
2020-03-20 13:24:34 +0900

    study jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function App() {
-  return <p>Hello World</p>;
+  const dom = <h1>Hello World!</h1>;
+  const text = 'I ma React.';
+  return (
+    <React.Fragment>
+      {dom}
+      <p>{text}</p>
+      <input type='text' onChange={() => { console.log('i am clicked!') }} />
+    </React.Fragment>
+  );
+
 }
 
 export default App;
