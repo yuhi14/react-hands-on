678d2101f807f6ef4249017b191112f09858c338
永田悠飛 <yuhi14skywalker@gmail.com>
2020-03-21 11:59:04 +0900

    study state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 const App = () => {
@@ -7,6 +7,8 @@ const App = () => {
       <WelcomeMessage title={'Hello World!'} sub={'I ma React.'} />
       <WelcomeMessage title={'Hello japan!'} sub={'I ma React.'} />
       <WelcomeMessage />
+      <p>------------------------------------------------------</p>
+      <Counter />
     </React.Fragment>
   );
 }
@@ -21,6 +23,29 @@ const WelcomeMessage = (props) => {
       <input type='text' onChange={() => { console.log('i am clicked!') }} />
     </React.Fragment>
   );
+}
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (<React.Fragment>
+      <div>count: {this.state.count}</div>
+      <button onClick={this.handlePlusButton}>+1</button>
+      <button onClick={this.handleMinusButton}>-1</button>
+    </React.Fragment>);
+  }
 }
 
 WelcomeMessage.defaultProps = {
